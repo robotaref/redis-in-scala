a2b238f66a14af67036cf6d89eaa6cb85e0e902d
Aref Ghodamai <aref.ghodamai@gmail.com>
2024-11-28 13:35:49 +0100

    codecrafters submit [skip ci]

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -1,12 +1,41 @@
 package codecrafters_redis
 
-import java.net.{InetSocketAddress, ServerSocket}
+import java.io._
+import java.net._
 
 object Server {
   def main(args: Array[String]): Unit = {
-    println("Logs from your program will appear here!")
     val serverSocket = new ServerSocket()
     serverSocket.bind(new InetSocketAddress("localhost", 6379))
-    val clientSocket = serverSocket.accept() // wait for client
+    while (true) {
+      val clientSocket = serverSocket.accept()
+      new Thread(() => handleClient(clientSocket)).start()
+    }
+  }
+
+  def handleClient(clientSocket: Socket): Unit = {
+    val in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream))
+    val out = new PrintWriter(clientSocket.getOutputStream, true)
+
+    try {
+      var running = true
+      while (running) {
+        val message = in.readLine()
+        if (message == null) {
+          running = false
+        } else {
+          println(s"Received: $message")
+          if (message == "PING") {
+            out.println("+PONG\r") // Respond to ping
+          }
+        }
+      }
+    } catch {
+      case e: IOException => println(s"Connection error: ${e.getMessage}")
+    } finally {
+      println(s"Closing connection with ${clientSocket.getInetAddress}")
+      clientSocket.close()
+    }
   }
 }
+
